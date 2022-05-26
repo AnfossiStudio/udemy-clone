804ca04bb0f4862fbc7a88b955a9221556dec9fa
unknown <maroc-hamza2016@hotmail.com>
2022-05-26 20:41:22 +0100

    Create PHP CLI for generate tables and controllers and models

---
+++ app/bin/cli.php
@@ -0,0 +1,115 @@
+<?php
+
+class CLI
+{
+
+  /**
+   * generate model file
+   */
+  public static function model($name)
+  {
+    $name = ucfirst(strtolower(trim($name)));
+    $template =
+      "<?php
+
+use App\Core\Model;
+
+class " . $name . " extends Model
+{
+  protected \$table = '" . strtolower($name) . "';
+}
+";
+    $path = __DIR__ . '/../models/' . $name . ".php";
+    if (!is_file($path)) {
+      file_put_contents($path, $template);
+      self::Log("model " . $name . " created successfuly \n");
+    } else {
+      self::Log("Model already exists", 'e');
+    }
+  }
+
+  /**
+   * generate table file
+   */
+  public static function table($name)
+  {
+    $name = strtolower(trim($name));
+    $template =
+      "<?php
+
+use App\Core\Schema;
+use App\Core\Table;
+
+class " . $name . "_table
+{
+  public static function build()
+  {
+    return new Table('" . $name . "', function () {
+      return [
+        Schema::id()
+      ];
+    });
+  }
+}";
+
+    $path = __DIR__ . '/../migrations/' . $name . "_table.php";
+    if (!is_file($path)) {
+      file_put_contents($path, $template);
+      self::Log("table " . $name . "_table created successfuly \n");
+    } else {
+      self::Log("Table already exists", 'e');
+    }
+  }
+
+  /**
+   * Genreate Controller file
+   */
+  public static function controller($name)
+  {
+
+    $name = ucfirst(strtolower(trim($name)));
+    $template =
+      "<?php
+
+use App\Core\Controller;
+
+class " . $name . " extends Controller
+{
+  public function index()
+  {
+  }
+}
+    ";
+    $path = __DIR__ . '/../controllers/' . $name . "Controller.php";
+    if (!is_file($path)) {
+      file_put_contents($path, $template);
+      self::Log("Controller " . $name . "_table created successfuly \n");
+    } else {
+      self::Log("Controller already exists", 'e');
+    }
+  }
+
+  /**
+   * Log for console
+   */
+  public static function Log($str, $type = 's')
+  {
+    switch ($type) {
+      case 'e': //error
+        echo "\n\033[31m$str \033[0m\n";
+        break;
+      case 's': //success
+        echo "\n\033[32m$str \033[0m\n";
+        break;
+      case 'w': //warning
+        echo "\n\033[33m$str \033[0m\n";
+        break;
+      case 'i': //info
+        echo "\n\033[36m$str \033[0m\n";
+        break;
+      default:
+        # code...
+        break;
+    }
+  }
+}
---
+++ app/bin/nano_cli.php
@@ -0,0 +1,59 @@
+<?php
+require 'cli.php';
+/**
+ * 
+ * migrate
+ * 
+ * //generate:model
+ * //generate:table
+ * generate:controller
+ * 
+ */
+
+
+
+unset($argv[0]);
+
+$arguments = $argv;
+
+/**
+ * Migrate table to database
+ */
+if (end($arguments) == "migrate") {
+  $tables = scandir(__DIR__ . "/../migrations/");
+  $tables = array_slice($tables, 2);
+  foreach ($tables as $table) {
+    require_once __DIR__ . '/../core/init.php';
+    require_once __DIR__  . '/../migrations/' . $table;
+    $class = explode('.', $table)[0];
+    $class::build();
+    CLI::Log("table " . $class . " migration successfuly \n");
+  }
+}
+
+/**
+ * 
+ * generate functions
+ */
+
+if (explode(':', $arguments[1])[0] === "generate") {
+  $command = explode(":", $arguments[1])[1];
+  $filename = $arguments[2] ?? null;
+  if (!$filename) {
+    CLI::Log("Please enter file name", 'e');
+    return;
+  }
+  switch ($command) {
+    case "table":
+      CLI::table($filename);
+      break;
+    case "model":
+      CLI::model($filename);
+      break;
+    case "controller":
+      CLI::controller($filename);
+      break;
+    default:
+      CLI::Log("Error can't find the command try again", 'e');
+  }
+}
